68e69e74fc4401175387cb9d8682c8426350fcc6
Rustam-Z <zokirovrustam202@gmail.com>
2020-08-20 10:21:22 +0500

    basic catalog done

--- myshop/settings.py
+++ myshop/settings.py
@@ -126,5 +126,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
